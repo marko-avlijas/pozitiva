cee7517abc802b83ae0eea37a7721a0839bfef20
Zoran M <zoran.majstorovic@gmail.com>
2014-03-31 00:43:57 +0200

    added to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,6 +31,7 @@ set :rbenv_ruby, '2.1.1'
 
 # Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/initializers/app_env.rb}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
