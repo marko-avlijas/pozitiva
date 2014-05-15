c8e6d73dd80a285a5b58dc5f0986e80fcabd65fc
Ejla <ejla1@me.com>
2014-05-15 10:00:02 +0200

    Mobile fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,16 @@
 
 module ApplicationHelper
 
+  MOBILE_USER_AGENTS =  'palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|' +
+                        'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
+                        'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
+                        'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
+                        'webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|' +
+                        'mobile|zune'
+  def mobile_device?
+    request.user_agent.to_s.downcase =~ Regexp.new(MOBILE_USER_AGENTS)
+  end
+  
   def error_messages_for(*objects)
     options = objects.extract_options!
     options[:header_message] ||= I18n.t(:"activerecord.errors.header", default: "Invalid Fields")
