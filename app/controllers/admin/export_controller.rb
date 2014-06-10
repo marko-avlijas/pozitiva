9a06b59166c3612213ef0120916dfabe7c451137
Ejla <ejla1@me.com>
2014-06-10 21:23:32 +0200

    Export

--- app/controllers/admin/export_controller.rb
+++ app/controllers/admin/export_controller.rb
@@ -1,10 +1,16 @@
 class Admin::ExportController < ApplicationController
-  layout 'table', only: [:index]
-  # before_filter :seed_admin
+  layout 'export', only: [:index]
   before_action :current_user_is_admin
   
   def index
-    @order_items = OrderItem.includes(order: :user).includes(offer_item: { offer: :user }).all
+    if params[:group_id].present? && params[:start_date].present? && params[:end_date].present?
+      if params[:commit] == "Generiraj tablicu"
+        @order_items = OrderItem.joins(order: {user: :group}).joins(order: :delivery).includes(offer_item: { offer: :user }).where("groups.id = ?", params[:group_id].to_i).where("deliveries.when >= ? AND deliveries.when <= ?", params[:start_date], params[:end_date].to_date+1.day)
+      else
+        @offer_items_without_orders = OfferItem.where("offer_items.id not in (select order_items.offer_item_id from order_items)").includes(offer: :user)
+      end
+    end
+    
   end
   
 end
