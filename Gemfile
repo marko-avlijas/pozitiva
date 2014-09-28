dde521fed66fcebe07ef39cd00776dcffa977f28
Ejla <ejla1@me.com>
2014-09-28 14:48:35 +0200

    rails bump

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 ruby "2.1.1"
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.1.1'
+gem 'rails', '4.1.5'
 
 group :development do
   # gem 'sqlite3'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,16 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-// require jquery.ui.all
-//= require jquery.ui.core
-//= require jquery.ui.widget
-//= require jquery.ui.mouse
-//= require jquery.ui.position
-//= require jquery.ui.draggable
-//= require jquery.ui.droppable
-//= require jquery.ui.resizable
-//= require jquery.ui.selectable
-//= require jquery.ui.sortable
+//= require jquery-ui
 
 //  require turbolinks
 //= require bootstrap
