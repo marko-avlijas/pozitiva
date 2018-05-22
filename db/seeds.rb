ea6da9a52bc13e54571229b2d1efd8b1a84d859c
Mirna Majstorovic <mirna.majstorovic@gmail.com>
2018-05-22 15:34:52 +0200

    Fix footer

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# User.where({email: 'mm@it.hr', name: 'Mirna', admin: true}).first_or_create!
-
 # Location.where({title: 'MSU', description: 'na parkingu sa istocne strane', lat: 45.77876, lng: 15.98270}).first_or_create!
 Location.where({title: 'Sloboština', description: 'tbd'}).first_or_create!
 Location.where({title: 'Dugave', description: 'tbd'}).first_or_create!
