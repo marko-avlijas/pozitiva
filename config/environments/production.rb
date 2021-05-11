ab703dd293547e533e0cfc7f8fb4e3dd758c28da
Marko Avlijaš <marko.avlijas@flownform.com>
2021-05-11 12:58:51 +0200

    Let sendgrid to use api key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ Pozitiva::Application.configure do
     :address              => 'smtp.sendgrid.net',
     :port                 => '587',
     :authentication       => :plain,
-    :user_name            => ENV['SENDGRID_USERNAME'],
-    :password             => ENV['SENDGRID_PASSWORD'],
+    :user_name            => 'apikey',
+    :password             => ENV['SENDGRID_API_KEY']
     :domain               => 'herokuapp.com',
     :enable_starttls_auto => true
   }
